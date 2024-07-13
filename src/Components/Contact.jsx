7ac4a515903e3786be62d77ae8c18c2c8fb3d2b1
EliasHania <eliasnumero1@gmail.com>
2024-07-13 21:43:10 +0200

    Eliminadas dependencias de Express y Nodemailer, ajustes en Contact component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,9 +4,8 @@ import { useInView } from 'react-intersection-observer';
 
 const Contact = () => {
   const [nombre, setNombre] = useState('');
-  const [email, setEmail] = useState('');
   const [mensaje, setMensaje] = useState('');
-  const [contacto, setContacto] = useState('email'); // Estado para elegir método de contacto
+  const [contacto, setContacto] = useState('whatsapp'); // Estado para elegir método de contacto
 
   const { ref, inView } = useInView({
     triggerOnce: false,
@@ -53,27 +52,7 @@ const Contact = () => {
       >
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-12 text-custom-yellow-2">Contáctanos</h2>
-          <div className="max-w-md mx-auto mb-8 shadow-2xl shadow-yellow-500/10">
-            <div className="rounded-lg overflow-hidden shadow-lg">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3222.055171807822!2d-5.4480173!3d36.14086989999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd0cebdc700a4375%3A0xeafd1aaa4c7fcc51!2sBarberia%20casanova!5e0!3m2!1ses!2ses!4v1720831733816!5m2!1ses!2ses"
-                width="600"
-                height="450"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                className="w-full h-full"
-              ></iframe>
-            </div>
-          </div>
           <div className="mb-4 flex justify-center space-x-4">
-            <button
-              className={`px-4 py-2 rounded ${contacto === 'email' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'}`}
-              onClick={() => handleContactoChange('email')}
-            >
-              Email
-            </button>
             <button
               className={`px-4 py-2 rounded ${contacto === 'whatsapp' ? 'bg-green-500 text-white' : 'bg-gray-300 text-gray-700'}`}
               onClick={() => handleContactoChange('whatsapp')}
@@ -87,46 +66,6 @@ const Contact = () => {
               Teléfono
             </button>
           </div>
-          {contacto === 'email' && (
-            <motion.form
-              className="max-w-md mx-auto"
-              onSubmit={handleSubmit}
-              initial={{ opacity: 0 }}
-              animate={inView ? { opacity: 1 } : { opacity: 0 }}
-              transition={{ duration: 0.5 }}
-            >
-              <div className="mb-4">
-                <input
-                  type="text"
-                  className="w-full p-2 rounded"
-                  placeholder="Nombre"
-                  value={nombre}
-                  onChange={(e) => setNombre(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <input
-                  type="email"
-                  className="w-full p-2 rounded"
-                  placeholder="Correo Electrónico"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <textarea
-                  className="w-full p-2 rounded"
-                  placeholder="Mensaje"
-                  value={mensaje}
-                  onChange={(e) => setMensaje(e.target.value)}
-                  required
-                ></textarea>
-              </div>
-              <button type="submit" className="px-4 py-2 rounded bg-custom-yellow-2 hover:bg-yellow-500 text-white">Enviar</button>
-            </motion.form>
-          )}
           {contacto === 'whatsapp' && (
             <motion.div
               className="flex justify-center mt-4"
